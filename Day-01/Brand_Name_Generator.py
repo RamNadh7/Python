216d6cd66cf01b8c711a8a8dcc4b52754df568ae
Ramanadham Pokuri <ramanadh99@gmail.com>
2025-05-04 17:09:47 +0530

    Day 1 Project

---
+++ Day-01/Brand_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Brand Name Generator!!")
+city=input("What's the Name of the city you grew up in?\n")
+pet=input("What's your pet name?\n")
+print("Your brand name coule be "+city+" "+ pet)
