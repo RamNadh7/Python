f2ceafe5bfc40a230095072375917481775f0cc0
Ramanadham Pokuri <ramanadh99@gmail.com>
2025-06-06 23:24:53 +0530

    Add initial content for Programming Fundamentals notebook with operators and data types

--- Day-01/Brand_Name_Generator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to Brand Name Generator!!")
-city=input("What's the Name of the city you grew up in?\n")
-pet=input("What's your pet name?\n")
-print("Your brand name coule be "+city+" "+ pet)
