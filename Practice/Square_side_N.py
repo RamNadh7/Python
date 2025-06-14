b6751653a9b296fc5dd8ccab85f5d21ecb7edb4a
Ramanadham Pokuri <ramanadh99@gmail.com>
2025-06-14 20:27:35 +0530

    Add square pattern generator script

---
+++ Practice/Square_side_N.py
@@ -0,0 +1,6 @@
+'''
+You are given an integer n. Your task is to print a square pattern of size n x n made up of the character '*', 
+represented as a list of strings.
+'''
+n=int(input('Enter the number:'))
+print([n*'*']*n)
